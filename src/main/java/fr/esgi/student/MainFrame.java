8a74a722d66a96c50078164e7c7f2b186b2adb90
Agence 369 <dev@agence369.com>
2023-04-04 18:03:20 +0200

    TG-64 #closed usage de weight pour remplir l'espace

--- src/main/java/fr/esgi/student/MainFrame.java
+++ src/main/java/fr/esgi/student/MainFrame.java
@@ -13,6 +13,9 @@ public class MainFrame extends JFrame {
         this.setSize(dimension);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // enleve le BorderLayout pour que l'ihm soit en haut a gauche
+        this.getContentPane().setLayout(null);
+
         initIHM();
 
 //        this.pack();
@@ -32,6 +35,9 @@ public class MainFrame extends JFrame {
         gbc.gridx = 0;
         gbc.gridy = 0;
 
+        gbc.weightx = 1.0;
+        gbc.weighty = 0.0;
+
         jPanel.add(new JLabel("Liste de todos a faire"), gbc);
 
         gbc.gridx = 0;
@@ -47,15 +53,22 @@ public class MainFrame extends JFrame {
         gbc.gridy = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
 
+        gbc.weightx = 2.0;
+
         jPanel.add(new JTextField("Nom du todo"), gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weighty = 1.0;
+
 
         jPanel.add(new JTextArea("Description du todo"), gbc);
 
+        jPanel.setLocation(0, 0);
+
         this.setContentPane(jPanel);
+
     }
 
     public void setTodos(List<String> todos) {
